ac08e6269a3b2ecaa98534dab06f312adb83e92a
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-02-06 19:15:48 -0300

    final project

---
+++ src/main/java/com/devsuperior/bds04/services/EventService.java
@@ -0,0 +1,61 @@
+package com.devsuperior.bds04.services;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.repositories.CityRepository;
+import com.devsuperior.bds04.repositories.EventRepository;
+import com.devsuperior.bds04.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Transactional(readOnly = true)
+    public Page<EventDTO> findAllPaged(Pageable pageable) {
+        Page<Event> list = repository.findAll(pageable);
+        return list.map(EventDTO::new);
+    }
+
+    @Transactional
+    public EventDTO update(Long id, EventDTO dto) {
+        try {
+            Event entity = repository.getOne(id);
+            copyDtoToEntity(dto, entity);
+            entity = repository.save(entity);
+            return new EventDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    @Transactional
+    public EventDTO insert(EventDTO dto) {
+        Event entity = new Event();
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new EventDTO(entity);
+    }
+
+    private void copyDtoToEntity(EventDTO dto, Event entity) {
+        entity.setName(dto.getName());
+        entity.setDate(dto.getDate());
+        entity.setUrl(dto.getUrl());
+
+        City city = cityRepository.getOne(dto.getCityId());
+        entity.setCity(city);
+    }
+}
